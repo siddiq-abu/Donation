0a01ac60a41de59a722b519d5acf7c2be0fbc3d5
siddiq-abu <abusiddiq200g3@gmail.com>
2023-10-17 09:53:00 +0000

    update

---
+++ app.js
@@ -0,0 +1,10 @@
+//steps
+// onclick show sign up forms to fill details
+
+function signUp(){
+
+    display = document.querySelector('.form-container');
+
+    display.classList.toggle('popup');
+
+}
